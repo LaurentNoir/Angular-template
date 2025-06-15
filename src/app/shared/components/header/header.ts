96bdb88a4ef0ce40e4b52095b0635be627de0345
Laurent Castañeda <laurentc@ditech.es>
2025-06-15 12:24:40 -0500

    header has been fixed

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  public isOpen: boolean = false;
+  public messajeIsOpen: boolean = false;
+  public alertIsOpen: boolean = false;
 
+  @ViewChild('userMenu')
+  public userMenuButton: ElementRef | undefined;
+
+  @ViewChild('mensajeMenu')
+  public mensajeMenuButton: ElementRef | undefined;
+
+  @ViewChild('alertMenu')
+  public alertMenuButton: ElementRef | undefined;
+
+  public openCloseUserMenu() {
+    this.isOpen = !this.isOpen;
+  }
+
+  public openCloseMessageMenu() {
+    this.messajeIsOpen = !this.messajeIsOpen;
+  }
+
+  public openCloseAlertMenu(){
+    this.alertIsOpen = !this.alertIsOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickFuera(event: MouseEvent) {
+    if (this.userMenuButton && !this.userMenuButton.nativeElement.contains(event.target)) {
+      this.isOpen = false;
+    }
+
+    if (this.mensajeMenuButton && !this.mensajeMenuButton.nativeElement.contains(event.target)) {
+      this.messajeIsOpen = false;
+    }
+
+    if (this.alertMenuButton && !this.alertMenuButton.nativeElement.contains(event.target)) {
+      this.alertIsOpen = false;
+    }
+
+  }
 }
--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
 export class Sidebar {
+  public isOpen: boolean = true;
 
+  public openCloseSidebar(){
+    this.isOpen = !this.isOpen;
+  }
 }
